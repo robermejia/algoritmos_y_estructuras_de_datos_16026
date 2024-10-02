b0c16aaddc80fd768f5e4f325e8f8ca5776edeb8
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2024-10-01 20:20:41 -0500

    Subiendo clase semana5.

--- src/Clase_4/Test_Arreglo_Bidimensional.java
+++ src/Clase_4/Test_Arreglo_Bidimensional.java
@@ -4,8 +4,9 @@ public class Test_Arreglo_Bidimensional {
 
     public static void main(String[] args) {
         
-    // RECORRIDO ARREGLO BIDIMENSIONAL
-    // ---------------------------------------------------------------------
+    // ################## RECORRIDO ARREGLO BIDIMENSIONAL ######################
+    // -------------------------------------------------------------------------
+    /*
     // Declaración e inicialización del arreglo bidimensional
         int[][] matriz = {
                 {1, 2, 3},
@@ -22,6 +23,130 @@ public class Test_Arreglo_Bidimensional {
             }
             System.out.println(); // Salto de línea después de cada fila
         }
+    */    
+    // ###################### MATRIZ TRIANGULAR INFERIOR #######################
+    // -------------------------------------------------------------------------  
+    /*
+        int n = 4; // Tamaño de la matriz cuadrada
+        int[][] matriz = new int[n][n];
 
+        // Inicializando la matriz triangular inferior
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i >= j) {
+                    matriz[i][j] = i + j; // Ejemplo simple de valor
+                } else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+        // Imprimiendo la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    */
+    // ###################### MATRIZ TRIANGULAR SUPERIOR #######################
+    // -------------------------------------------------------------------------  
+    /*
+        int n = 4; // Tamaño de la matriz cuadrada
+        int[][] matriz = new int[n][n];
+
+        // Inicializando la matriz triangular superior
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j) {
+                    matriz[i][j] = i + j; // Ejemplo simple de valor
+                } else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+        // Imprimiendo la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    */    
+    // ####################### MATRIZ TRIANGULAR TRIDIAGONAL ###################
+    // -------------------------------------------------------------------------
+    /*
+        int n = 5; // Tamaño de la matriz
+        int[][] matriz = new int[n][n];
+
+        // Inicializando la matriz tridiagonal
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (Math.abs(i - j) <= 1) {
+                    matriz[i][j] = i + j; // Ejemplo simple de valor
+                } else {
+                    matriz[i][j] = 0;
+                }
+            }
+        }
+
+        // Imprimiendo la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    */    
+    // ####################### MATRIZ TRIANGULAR SIMÉTRICAS ####################
+    // -------------------------------------------------------------------------
+    /*
+        int n = 4; // Tamaño de la matriz cuadrada
+        int[][] matriz = new int[n][n];
+
+        // Inicializando la matriz simétrica
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j) {
+                    matriz[i][j] = i + j; // Ejemplo simple de valor
+                    matriz[j][i] = matriz[i][j]; // Asegurando la simetría
+                }
+            }
+        }
+
+        // Imprimiendo la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    */    
+    // ###################### MATRIZ TRIANGULAR ASIMÉTRICA #####################
+    // -------------------------------------------------------------------------
+        int n = 4; // Tamaño de la matriz cuadrada
+        int[][] matriz = new int[n][n];
+
+        // Inicializando la matriz asimétrica
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) { // Diagonal principal con ceros
+                    matriz[i][j] = 0;
+                } else if (i < j) { // Parte triangular superior
+                    matriz[i][j] = i + j; // Ejemplo simple de valor
+                } else { // Parte triangular inferior
+                    matriz[i][j] = i * j; // Ejemplo simple de valor diferente a la parte superior
+                }
+            }
+        }
+
+        // Imprimiendo la matriz
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ src/Clase_5_Practica_Calificada/Solo_Hubo_Examen.java
@@ -0,0 +1,5 @@
+package Clase_5_Practica_Calificada;
+
+public class Solo_Hubo_Examen {
+    // NO HUBO TEMAS
+}
